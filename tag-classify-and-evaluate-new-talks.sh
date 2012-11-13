c649104f112361a87a7f67601b6357fcf219b9a8
Chris Beer <cabeer@stanford.edu>
2012-11-13 15:56:38 -0800

    tweak training

--- tag-classify-and-evaluate-new-talks.sh
+++ tag-classify-and-evaluate-new-talks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+rm -rf data/mallet
 mkdir data/mallet
 ruby divide-documents-by-acceptance.rb
-mallet import-dir --input data/partitioned_talks/* --output data/mallet/all-talks.mallet
-mallet train-classifier --trainer MaxEnt --input data/mallet/all-talks.mallet --training-portion 0.9 --report train:accuracy test:f1:accepted --output-classifier data/mallet/classifier
+mallet import-dir --remove-stopwords --input data/partitioned_talks/* --output data/mallet/all-talks.mallet
+mallet train-classifier --trainer MaxEnt --input data/mallet/all-talks.mallet --training-portion 0.85 --output-classifier data/mallet/classifier
 ruby dump-documents-one-per-file.rb
 
 mallet classify-dir --input data/talks --output data/mallet/classified-talks --classifier data/mallet/classifier
