2a11507e5f3e7f7c446a8d27cf57d1913f726a51
Chris Beer <cabeer@stanford.edu>
2012-11-13 16:04:54 -0800

    add topic classifications

---
+++ topic-classification-of-talks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUM_TOPICS=25
+NUM_TOP_WORDS=15
+
+rm -rf data/mallet
+mkdir data/mallet
+mallet import-dir --input data/talks --output data/mallet/talks.mallet --keep-sequence --remove-stopwords
+mallet train-topics --input data/mallet/talks.mallet --output-model data/mallet/talks-topic-model.gz --num-topics $NUM_TOPICS --num-threads 2 --num-iterations 1000 --doc-topics-threshold 0.1 --optimize-interval 10 --num-top-words $NUM_TOP_WORDS --output-doc-topics data/mallet/topics.txt --output-topic-keys data/mallet/keys.txt
+
